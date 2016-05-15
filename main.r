031513d3b6f8b7e99f60e7d89a85d9313f46e892
Nikita Kuznetsov <oychorange@gmail.com>
2016-05-15 04:24:51 +0300

    Update main.r

--- main.r
+++ main.r
@@ -1,64 +1,61 @@
 source("funcs.R")
 
+### gathering data
 x_source <- import_data(load_prev_result = T,addCountable = F)
-x <- addCountable(x_source)
-x_backup <- x
-nm <- namesM(x)
+x <- addCountable(x_source,plot_saudi = F)
+exploreData(x,corPlotOrder = F)
 
-# main
-nx1 <- normalizeM(x,save_sign = F); nx2 <- normalizeM(x,save_sign = T)
-exploreData(x)
-exploreNData(cbind(x)[,c(1,44:45)],1,legend_position = 'topleft',main='S: 2 options')
-scatterPlots(x)
-
-# regression data
-rx <- x[,c(30:ncol(x))]
-rx <- rx[,-c(9:11,13)]
+### gathering data for regression
+rx <- x[,c(30:ncol(x))] #30 = 1st counted column
+corrPlot(rx[,c(8:13)],order=F)
+rx <- rx[,-c(8:12)]
 rx$disPfut <- c(rx$disP[2:nrow(rx)],NA)
 rx <- addDiff(rx,l = c(1:ncol(rx)))
 rx <- cbind(d_disPfut=rx[,ncol(rx)],rx[,-c(ncol(rx),ncol(rx)/2)])
-nm <- namesM(rx); nm
-rx[,1] <- log(1+rx[,1])
-rx[,21] <- log(1+rx[,21])
-rx[,22] <- log(1+rx[,22])
-rx[,23] <- log(1+rx[,23])
 
-corrPlot(rx[,c(2:13)],order=F)
-
-# data root exploration
+### are stationary?
 rad <- adftestM(rx[2:(nrow(rx)-1),]); rad
 toChange <- (rad[,3]==F)*(1:nrow(rad)); toChange <- toChange[toChange!=0]
+  
+  # option 1: IF exclude not stationary
+  rx<-rx[2:(nrow(rx)-1),-toChange]
+  rad <- adftestM(rx); rad; nm <- names(rx)
+  
+  # option 2: IF change not stationary to diff(log(1+x)), trying make them stationary
+  #    rx <- addDiffLog(rx,toChange,1)[2:nrow(rx),]
+  #    rad <- adftestM(rx[2:(nrow(rx)-1),); rad
+  #    rx<-rx[2:(nrow(rx)-1),rad[,3]==T]
+  #    rad<-adftestM(rx);rad
 
-# IF exclude not stationary
-rx<-rx[2:(nrow(rx)-1),-toChange]
-rad <- adftestM(rx); rad; nm <- names(rx)
-
-# IF change not stationary to diff(log(1+x)), trying make them stationary
-    rx <- addDiffLog(rx,toChange,1)[2:nrow(rx),]
-    rad <- adftestM(rx[2:(nrow(rx)-1),]); rad
-    rx<-rx[2:(nrow(rx)-1),rad[,3]==T]
-    rad<-adftestM(rx);rad
-
-# step-by-step-excluding
-# trying different models
-variablesException(normalizeM(rx))[2:3]
-variablesException(normalizeM(rx[,-c(7,8)]))[2:3]
-variablesException(normalizeM(rx[,-c(22,23)]))[2:3]
-variablesException(normalizeM(rx[,-c(21,23)]))[2:3]
-variablesException(normalizeM(rx[,-c(21,22)]))[2:3]
-variablesException(normalizeM(rx[,-c(21)]))[2:3]
-
-variablesException(normalizeM(rx[,-c(22)]))[2:3]
-variablesException(normalizeM(rx[,-c(23)]))[2:3]
-variablesException(normalizeM(rx[,-c(10,23,17,7,8)]))[2:3]
-variablesException(normalizeM(rx[,-c(10,22)]))[2:3]
-variablesException(normalizeM(rx[,-c(7)]))[2:3]
-variablesException(normalizeM(rx[,-c(8)]))[2:3]
+### correlation problem
+rxt<-rx[,-w(rx,c("PNLComm","PNLSpec"))]
+rxt<-rxt[,-w(rx,c("d_STIndex","d_SSpecDegree","d_PercentDiff","d_PNLComm","d_PNLSpec"))]
+rxt <- rxt[-w(rxt,c("STIndex","SpecDegree","TIndex"))]
+rxt <- rxt[,-w(rxt,c("d_STIndex","d_PNLComm"))]
+v <- vif_test(rxt)
+rxb<-rx; rx<-rxt; # moving rx to backup; temp rx to rx
+corrPlot(rx,order=F)
 
 
+### building regression by step-by-step-excluding 
+# place for trying different models
+variablesException(rx[,],normalize=F)[1:3]
+summary(lm(rx$d_disPfut~rx$BalWorld+rx$disP+rx[,2])) #...
+summary(lm(rx$d_disPfut~rx$BalWorld+rx$d_PNLSpec+rx$TS14+rx$d_TS14+rx$disP))
 
 
-# why nonreportable belong to noncommercial
+# check every possible combination with at least 1 included variable
+res <- lm_check(rx,c(3,6))
+
+### moving window
+movingFixed(rx[,c(1,3,6)],x$date,years = 5)
+movingStepByStepExcluding(rx,x$date,5,par=c(3,2),plotPval = F,progressBar=T)
+
+
+
+### NonReportable behavior
+
+scatterPlots(x)
 
 oi <- data.frame(date=x$date,Commercial = x$CommercialLong-x$CommercialShort,NonCommercial=x$NoncommercialLong-x$NoncommercialShort,NonReportable=x$NonreportablePositionsLong-x$NonreportablePositionsShort)
 cor(oi[,2:4])
@@ -73,4 +70,3 @@ r <- NULL
 for (i in w:nrow(x)) r<-c(r,cor(oi$Commercial[(i-w+1):i],oi$NonReportable[(i-w+1):i]))
 oi_corr_dynamics$Commercial <- r
 exploreNData(data.frame(oi_corr_dynamics,price=nx2[w:nrow(x),5]),1,ymin=-1.5,main='Moving correlation between NonReportable Open Interest and others OI')
-
